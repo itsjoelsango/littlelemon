7ad11998b7f20b92e80747d60cadc67ed5a03160
itsjoelsango <itsjoelsango@gmail.com>
2023-02-24 15:36:59 +0200

    Removed file that shouldn't be tracked

---
+++ LittleLemon/Restaurant.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Jo Michael on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
