4de75d9ea7b88ab7bf0de6be82cfa3d80b870ea9
itsjoelsango <itsjoelsango@gmail.com>
2023-03-13 17:05:23 +0200

    Add Validation Feature to registration Form on Onboarding.swift file

--- LittleLemon/Onboarding.swift
+++ LittleLemon/Onboarding.swift
@@ -18,49 +18,168 @@ struct Onboarding: View {
     @State private var email = ""
     
     @State private var isLoggedIn = false
+    @State private var showFormInvalidMessage = false
+    @State private var errorMessage = ""
     
     var body: some View {
         NavigationView {
             VStack {
-                NavigationLink(destination: Home(), isActive: $isLoggedIn) {
-                    EmptyView()
-                }
-                .navigationTitle("Sign up")
-                
-                VStack {
-                    TextField("First Name", text: $firstName)
-                    TextField("Last Name", text: $lastName)
-                    TextField("Email", text: $email)
-                }
-                .textInputAutocapitalization(.never)
-                .autocorrectionDisabled(true)
-                
-                Button("Register") {
-                    if !firstName.isEmpty, !lastName.isEmpty, !email.isEmpty {
+                ZStack {
+
+                    VStack(alignment: .leading) {
+                        Text("Little Lemon")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(Color("Yellow"))
                         
-                        if isValidEmailAddress(email: email) {
-                            UserDefaults.standard.set(firstName, forKey: keyFirstName)
-                            UserDefaults.standard.set(lastName, forKey: keyLastName)
-                            UserDefaults.standard.set(email, forKey: keyEmail)
-                            UserDefaults.standard.set(true, forKey: keyIsLoggedIn)
-                            isLoggedIn = true
-                        } else {
-                            print("Invalid Email")
+                        Text("Chicago")
+                            .font(.subheadline)
+                        
+                        HStack(spacing: 20) {
+                            Text("We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
+                                .font(.subheadline)
+                            
+                            Image("Hero image")
+                                .resizable()
+                                .clipped()
+                                .frame(width: 112, height: 142)
+                                .cornerRadius(16)
                         }
-                    } else {
-                        print("Empty field found!")
+                    }
+                    .padding(20)
+                }
+                .background(Color("Green"))
+                
+                ScrollView {
+                    VStack {
+                        NavigationLink(destination: Home(), isActive: $isLoggedIn) {
+                            EmptyView()
+                        }
+                        .navigationTitle("Sign up")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .toolbar {
+                            ToolbarItem(placement: .principal) {
+                                Image("Logo")
+                            }
+                        }
+                        
+                        VStack(spacing: 20) {
+                            VStack(alignment:.leading) {
+                                Text("First Name *")
+                                    .font(.subheadline)
+                                    .foregroundColor(.red)
+                                
+                                TextField("Your first name", text: $firstName)
+                            }
+                            
+                            VStack(alignment:.leading) {
+                                Text("Last Name *")
+                                    .font(.subheadline)
+                                    .foregroundColor(.red)
+                                
+                                TextField("Your last Name", text: $lastName)
+                            }
+                            
+                            VStack(alignment:.leading) {
+                                Text("Email *")
+                                    .font(.subheadline)
+                                    .foregroundColor(.red)
+                                
+                                TextField("Email", text: $email)
+                            }
+                        }
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(true)
+                        .padding(.bottom, 20)
+                        
+                        // Register button
+                        Button(action: {
+                            validateForm() // Done
+                        }, label: {
+                            Text("REGISTER")
+                        })
+                        .buttonStyle(.bordered)
+                        .padding([.all], 5)
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                    }
+                    .textFieldStyle(.roundedBorder)
+                    .onAppear {
+                        if UserDefaults.standard.bool(forKey: keyIsLoggedIn) {
+                            isLoggedIn = true
+                        }
                     }
                 }
-                .buttonStyle(.bordered)
-            }
-            .textFieldStyle(.roundedBorder)
-            .padding()
-            .onAppear {
-                if UserDefaults.standard.bool(forKey: keyIsLoggedIn) {
-                    isLoggedIn = true
-                }
+                .padding()
+                .alert("ERROR", isPresented: $showFormInvalidMessage, actions: {
+                    Button("OK", role: .cancel) { }
+                }, message: {
+                    Text(self.errorMessage)
+                })
             }
         }
+    }
+    
+    private func validateForm () {
+        
+        // customerName must contain just letters
+        let firstNameIsValid = isValid(firstName: firstName)
+        let lastNameIsValid = isValid(lastName: lastName)
+        let emailIsValid = isValidEmailAddress(email: email)
+        
+        guard firstNameIsValid && lastNameIsValid && emailIsValid
+        else {
+            var invalidFirstNameMessage = ""
+            if firstName.isEmpty || !isValid(firstName: firstName) {
+                invalidFirstNameMessage = "First Name can only contain letters and must have at least 3 characters\n\n"
+            }
+            
+            var invalidLastNameMessage = ""
+            if lastName.isEmpty || !isValid(lastName: lastName) {
+                invalidLastNameMessage = "Last Name can only contain letters and must have at least 3 characters\n\n"
+            }
+            
+            var invalidEmailMessage = ""
+            if email.isEmpty || !isValidEmailAddress(email: email) {
+                invalidEmailMessage = "The e-mail is invalid and cannot be blank."
+            }
+            
+            self.errorMessage = "Found these erros in the form:\n\n \(invalidFirstNameMessage)\(invalidLastNameMessage)\(invalidEmailMessage)"
+            
+            showFormInvalidMessage.toggle()
+            return
+        }
+        
+        // Form is Valid, proceed
+        
+        // Save data with UserDefaults
+        UserDefaults.standard.set(firstName, forKey: keyFirstName)
+        UserDefaults.standard.set(lastName, forKey: keyLastName)
+        UserDefaults.standard.set(email, forKey: keyEmail)
+        UserDefaults.standard.set(true, forKey: keyIsLoggedIn)
+        self.isLoggedIn.toggle()
+        
+    }
+    
+    private func isValid(firstName: String) -> Bool {
+        guard !firstName.isEmpty, firstName.count > 2 else { return false }
+        for chr in firstName {
+            if (!(chr >= "a" && chr <= "z") && !(chr >= "A" && chr <= "Z") && !(chr == " ") ) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    private func isValid(lastName: String) -> Bool {
+        guard !lastName.isEmpty, lastName.count > 2 else { return false }
+        for chr in lastName {
+            if (!(chr >= "a" && chr <= "z") && !(chr >= "A" && chr <= "Z") && !(chr == " ") ) {
+                return false
+            }
+        }
+        return true
     }
     
     private func isValidEmailAddress(email: String) -> Bool {
